5bc034b8d0ce1278ac7c3f96aae5d674f6581318
Minho Kang <rkdalsgh32@gmail.com>
2023-03-04 13:09:07 +0900

    Add new lines for preparing data

--- classifier on text data.R
+++ classifier on text data.R
@@ -3,3 +3,12 @@ library(naivebayes)
 library(class)
 library(e1071)
 
+DTM <- read.csv('https://raw.githubusercontent.com/Minhokg/text-mining-r/main/ReuterDTM.csv',
+         header = T, row.names = 1)
+df <- DTM[,-1]
+lbls <- data.frame(DTM[,1])
+grps <- sort(unique(lbls[,1]))
+indexes <- lbls[,1]
+
+sum(colnames(df) %in% c(stopwords()))
+
